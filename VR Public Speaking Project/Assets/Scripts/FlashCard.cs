c78ed7b487f8b61399048900dc38354436f4d91d
Andrew Park <joohyung122002@gmail.com>
2025-03-18 17:37:20 -0700

    project additions

---
+++ VR Public Speaking Project/Assets/Scripts/FlashCard.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class FlashCard : MonoBehaviour
+{
+    /*
+   fruits
+ press enter or any button to go to next card  
+   food
+   
+   piano 
+    
+    */
+
+    List<string> cards = new List<string>();
+    //  0       1       2
+    // ["asdf", "food", "piano"];
+    int currentCardIndex = 0;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
