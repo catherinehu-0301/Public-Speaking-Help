9fd4c4605e63fc4ec5f4ebae1797f4bc8ad545b4
catherinehu-0301 <serenitysdestiny3@gmail.com>
2025-09-11 18:59:54 -0700

    website stuff

---
+++ Web Companion Server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+app.use(bodyParser.json({ limit: '2mb' }));
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+
+const DB_FILE = path.join(__dirname, 'data', 'db.json');
+
+// TODO: database functions
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
